3fad2d1509bc22d56eed2c3a26559e36047c04ec
IIIDelay <IIIDev@.com>
2023-08-02 23:16:48 +0800

    提交信息: PageEntity 提交人: IIIDev

--- YMail/ymall-common/commons-services/src/main/java/org/ymall/commons/cms/common/entity/PageEntity.java
+++ YMail/ymall-common/commons-services/src/main/java/org/ymall/commons/cms/common/entity/PageEntity.java
@@ -1,62 +1,51 @@
-
-
-
-
 /*
  * Copyright (c) 2023. 版权归III_Delay所有
  */
 
 package org.ymall.commons.cms.common.entity;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.Data;
+
+import java.util.List;
 
 /**
- * 简易分页实体
- *
-
+ * 分页对象
  */
-public class PageEntity {
-    private long current = 1;
-    private long size = 10;
-
-    public PageEntity() {
-    }
-
-    public PageEntity(long current, long size) {
-        this.current = current;
-        this.size = size;
-    }
-
-    public long getCurrent() {
-        return current;
-    }
-
-    public void setCurrent(long current) {
-        this.current = current;
-    }
-
-    public long getSize() {
-        return size;
-    }
-
-    public void setSize(long size) {
-        this.size = size;
-    }
+@Data
+public class PageEntity<OUT> {
+    /**
+     * total 总条数
+     */
+    private long total;
 
     /**
-     * 转换为MybatisPlus分页对象
-     *
-     * @return 分页实体
+     * size 每页数量
      */
-    public <T> Page<T> toPage() {
-        return new Page<>(current, size);
-    }
+    private long size;
 
-    @Override
-    public String toString() {
-        return "PageEntity{" +
-            "current=" + current +
-            ", size=" + size +
-            '}';
+    /**
+     * current 当前页
+     */
+    private long current;
+
+    /**
+     * totalPageSize 总页数
+     */
+    private long totalPageSize;
+
+    /**
+     * records 分页内容
+     */
+    private List<OUT> records;
+    
+    /**
+     * hasNextPage 是否有下页
+     */
+    private boolean hasNextPage;
+
+    public void hasNextPage() {
+        if (current*size < total) {
+            setHasNextPage(true);
+        }
     }
 }
