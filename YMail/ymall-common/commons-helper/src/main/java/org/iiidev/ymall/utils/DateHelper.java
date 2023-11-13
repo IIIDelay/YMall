93ad9ab093d67ef256b43d1ca2507288fae3a9d7
IIIDelay <IIIDev@.com>
2023-11-13 22:48:32 +0800

    提交信息: UPDATE 提交人: This0903

--- YMail/ymall-common/commons-helper/src/main/java/org/iiidev/ymall/utils/DateHelper.java
+++ YMail/ymall-common/commons-helper/src/main/java/org/iiidev/ymall/utils/DateHelper.java
@@ -34,6 +34,20 @@ public class DateHelper {
         return Date.from(instant);
     }
 
+    /**
+     * beginOfDayEQ : 包含今日
+     *
+     * @param offsetDay offsetDay
+     * @return Date
+     */
+    public static Date beginOfDayEQ(int offsetDay) {
+        return beginOfDay(offsetDay + 1);
+    }
+
+    public static Date endOfDayEQ(int offsetDay) {
+        return endOfDay(offsetDay + 1);
+    }
+
     public static Date endOfDay(int offsetDay) {
         // Instant instant = LocalDateTime.of(LocalDate.now().minusDays(offsetDay), LocalTime.MIN).atZone(ZoneId.systemDefault()).toInstant();
         Instant instant = LocalDateTime.now().plusDays(offsetDay).with(LocalTime.MAX).atZone(ZoneId.systemDefault()).toInstant();
@@ -46,14 +60,18 @@ public class DateHelper {
     }
 
     public static void main(String[] args) {
-        Date date = beginOfDay(-1);
-        Date date1 = endOfDay(-1);
-        String format = DateUtil.format(date, "yyyy-MM-dd HH:mm:ss");
-        String format1 = DateUtil.format(date1, "yyyy-MM-dd HH:mm:ss");
-        System.out.println("format = " + format);
-        System.out.println("format1 = " + format1);
+        // Date date = beginOfDay(-1);
+        // Date date1 = endOfDay(-1);
+        // String format = DateUtil.format(date, "yyyy-MM-dd HH:mm:ss");
+        // String format1 = DateUtil.format(date1, "yyyy-MM-dd HH:mm:ss");
+        // System.out.println("format = " + format);
+        // System.out.println("format1 = " + format1);
+        //
+        // Instant instant = LocalDateTime.now().withDayOfMonth(1).with(LocalTime.MIN).atZone(ZoneId.systemDefault()).toInstant();
+        // System.out.println("Date.from(instant) = " + Date.from(instant));
 
-        Instant instant = LocalDateTime.now().withDayOfMonth(1).with(LocalTime.MIN).atZone(ZoneId.systemDefault()).toInstant();
-        System.out.println("Date.from(instant) = " + Date.from(instant));
+        Date date = beginOfDay(0);
+        String format = DateUtil.format(date, "yyyy-MM-dd HH:mm:ss");
+        System.out.println("format = " + format);
     }
 }
